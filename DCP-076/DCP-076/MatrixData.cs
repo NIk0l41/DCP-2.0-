1df721d8d4ae051f45cb5e1f0a60eaa5df6ffb87
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-09 09:12:20 +1000

    DCP-030
    Pretty happy with this result!
    It did take me a bit to create the MatrixData class, but in the end it makes for a nice, clean program.

---
+++ DCP-076/DCP-076/MatrixData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCP_076
+{
+    class MatrixData
+    {
+        public char[,] matrix;
+        public int[,] matrixInt;
+
+        public int nColumn = 0;
+        public int mRow = 0;
+
+        public MatrixData(int choice) {
+            switch (choice) {
+                case 1:
+                    nColumn = 3;
+                    mRow = 3;
+                    matrix = new char[3, 3]{ {'c', 'd', 'g' },{'b', 'a', 'h' },{ 'a', 'f', 'i' } };
+                    matrixInt = new int[3, 3];
+                    break;
+                case 2:
+                    nColumn = 2;
+                    mRow = 3;
+                    matrix = new char[2, 3] { { 'c', 'd', 'g' },{ 'b', 'a', 'h' } };
+                    matrixInt = new int[2, 3];
+                    break;
+                case 3:
+                    nColumn = 1;
+                    mRow = 6;
+                    matrix = new char[1, 6] { { 'a', 'b', 'c', 'd', 'e', 'f' } };
+                    matrixInt = new int[1, 6];
+                    break;
+                case 4:
+                    nColumn = 3;
+                    mRow = 3;
+                    matrix = new char[3, 3] { {'z', 'w', 't' },{ 'y', 'v', 'u'},{ 'x', 'u', 'r' } };
+                    matrixInt = new int[3, 3];
+                    break;
+                default:
+                    nColumn = 1;
+                    mRow = 1;
+                    matrix = new char[1, 1];
+                    matrixInt = new int[1, 1];
+                    break;
+            }
+            //Populate MatrixInt
+            for (int nCo = 0; nCo < nColumn; nCo++) {
+                for (int mRo = 0; mRo < mRow; mRo++) {
+                    matrixInt[nCo, mRo] = ReturnLetterValue(matrix[nCo, mRo]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Input Char, Output Integer.
+        /// </summary>
+        /// <param name="inputChar">Input Letter.</param>
+        /// <returns>Alphabet Position of Input Letter.</returns>
+        int ReturnLetterValue(char inputChar)
+        {
+            switch (inputChar)
+            {
+                case 'a':
+                    return 1;
+                case 'b':
+                    return 2;
+                case 'c':
+                    return 3;
+                case 'd':
+                    return 4;
+                case 'e':
+                    return 5;
+                case 'f':
+                    return 6;
+                case 'g':
+                    return 7;
+                case 'h':
+                    return 8;
+                case 'i':
+                    return 9;
+                case 'j':
+                    return 10;
+                case 'k':
+                    return 11;
+                case 'l':
+                    return 12;
+                case 'm':
+                    return 13;
+                case 'n':
+                    return 14;
+                case 'o':
+                    return 15;
+                case 'p':
+                    return 16;
+                case 'q':
+                    return 17;
+                case 'r':
+                    return 18;
+                case 's':
+                    return 19;
+                case 't':
+                    return 20;
+                case 'u':
+                    return 21;
+                case 'v':
+                    return 22;
+                case 'w':
+                    return 23;
+                case 'x':
+                    return 24;
+                case 'y':
+                    return 25;
+                case 'z':
+                    return 26;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ DCP-076/DCP-076/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace DCP_076
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What example matrix would you like to try out?");
+            Console.WriteLine("Enter an integer value between 1 and 4 (inclusive).");
+            try {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice > 0 || choice < 5)
+                {
+                    MatrixData data = new MatrixData(choice);
+                    int[] dimensions = { data.nColumn, data.mRow };
+                    int nColumn = dimensions[0];
+                    int mRow = dimensions[1];
+                    char[,] matrix = data.matrix;
+                    int[,] matrixInt = data.matrixInt;
+
+                    PrintCharacterMatrix(matrix, dimensions);
+                    PrintIntegerMatrix(matrixInt, dimensions);
+
+                    Console.WriteLine("Given the current Matrix, " + FindDeathMarks(matrixInt, dimensions) + " column(s) need to be deleted.");
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("Please enter a value within the bounds next time.");
+                    Console.WriteLine("Thank you.");
+                    Console.ReadLine();
+                }
+            }
+            catch {
+                Console.WriteLine("Please enter an integer value.");
+                Console.ReadLine();
+            }
+        }
+
+
+        
+        /// <summary>
+        /// Finds DeathMarks. Duh.
+        /// </summary>
+        /// <param name="matrixInt">Input Integer Matrix to Peruse.</param>
+        /// <returns>The number of columns to be deleted.</returns>
+        static int FindDeathMarks(int[,] matrixInt, int[] dims) {
+            int[] markedForDeath = new int[dims[0]];
+            for (int nColumn = 0; nColumn < dims[0]; nColumn++) {
+                for (int mRow = 1; mRow < dims[0]; mRow++) {
+                    if (matrixInt[nColumn, mRow] <= matrixInt[nColumn, mRow - 1]){
+                        //Console.WriteLine(matrixInt[nColumn, mRow] + " <= " + matrixInt[nColumn, mRow - 1]);
+                        markedForDeath[nColumn] = 1;
+                    }
+                    //Console.WriteLine("The value at: [" + nColumn + ", " + mRow + "] is " + matrixInt[nColumn, mRow]);
+                }
+            }
+            int totalRowsToDelete = 0;
+            for (int marks = 0; marks < markedForDeath.Length; marks++) {
+                //Console.WriteLine("markedForDeath[" + marks + "] = " + markedForDeath[marks]);
+                totalRowsToDelete += markedForDeath[marks];
+            }
+            return totalRowsToDelete;
+        }
+
+        /// <summary>
+        /// Inputs 2D Integer Array, Prints contents on Console.
+        /// </summary>
+        /// <param name="matrixInt">2D Integer Array to Print</param>
+        static void PrintIntegerMatrix(int[,] matrixInt, int[] dims) {
+            Console.WriteLine("---Integer Matrix---");
+            for (int mRow = 0; mRow < dims[1]; mRow++) {
+                for (int nColumn = 0; nColumn < dims[0]; nColumn++) {
+                    if (nColumn == dims[1] - 1) {
+                        Console.Write(matrixInt[nColumn, mRow]);
+                    }
+                    else {
+                        Console.Write(matrixInt[nColumn, mRow] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Inputs 2D Character Array, Prints contents on Console.
+        /// </summary>
+        /// <param name="matrix">2D Char Array to Print.</param>
+        static void PrintCharacterMatrix(char[,] matrix, int[] dims) {
+            Console.WriteLine("---Character Matrix---");
+            for (int mRow = 0; mRow < dims[1]; mRow++) {
+                for (int nColumn = 0; nColumn < dims[0]; nColumn++) {
+                    if (nColumn == dims[1] - 1) {
+                        Console.Write(matrix[nColumn, mRow]);
+                    }
+                    else {
+                        Console.Write(matrix[nColumn, mRow] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
