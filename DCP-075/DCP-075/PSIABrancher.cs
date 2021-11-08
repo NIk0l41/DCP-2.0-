61940f234fa9595e99e49653ea26dedd692de9aa
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-08 18:22:08 +1000

    DCP-075
    A Fever Dream to be sure, but a welcome one.

---
+++ DCP-075/DCP-075/PSIABrancher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCP_075
+{
+    class PSIABrancher
+    {
+        /// <summary>
+        /// Returns the length of the longest possible subsequent increasing array.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <returns>An integer length.</returns>
+        public int DetermineLongestPSIA(int[] array) {
+            /* //Determine Starting Points
+             * Each iteration of this loop chooses a new point to start at.
+             * May be in our interest to keep this in mind.
+             */
+            int length = 0;
+            int[] PSIA = { };
+            for (int startingPoint = 0; startingPoint < array.Length; startingPoint++) {
+                int tmp = IterateBranch(array, startingPoint, 0);
+                length = (tmp > length) ? tmp : length;
+            }
+
+            return length;
+        }
+
+        public int IterateBranch(int[] array, int startPoint, int counter) {
+            int[] possibleBranches = { };
+            //Foreach item after startpoint, check if it's valid for PSIA
+            for (int possibleBranch = startPoint + 1; possibleBranch < array.Length; possibleBranch++) {
+                if (array[possibleBranch] > array[startPoint]) {
+                    //If the possibleBranch is valid, add the position to possibleBranches.
+                    possibleBranches = possibleBranches.Append(possibleBranch);
+                }
+            }
+            //If there are no possibleBranches that could make a PSIA,
+            //We return how many steps were taken prior.
+            //No progress is made.
+            if (possibleBranches.Length == 0) {
+                return counter;
+            }
+            else {
+                //If there are possibleBranches that could make a PSIA, 
+                //we need to IterateBranch, and see how the counts go.
+                for (int branches = 0; branches < possibleBranches.Length; branches++) {
+                    int branchCount = IterateBranch(array, possibleBranches[branches], counter + 1);
+                    counter = (branchCount > counter) ? branchCount : counter;
+                }
+
+                return counter;
+
+            }
+        }
+
+
+    }
+
+}
---
+++ DCP-075/DCP-075/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCP_075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Longest Output is 6 --> 0, 2, 6, 9, 11, 15
+            int[] input = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+            int[] coll = new int[1];
+            PSIABrancher brancher = new PSIABrancher();
+            int length = brancher.DetermineLongestPSIA(input);
+            Console.WriteLine("Length: " + length);
+            Console.ReadLine();
+
+            //for (int i = 0; i < input.Length; i++) {
+                //int[] tmp = PointStart(0, input);
+                //if (tmp.Length > coll.Length) {
+                    //coll = tmp;
+                //}
+            //}
+            //Console.WriteLine("Coll");
+            //PrintArray(113, coll);
+            //Console.WriteLine("Length: " + coll.Length);
+        }
+        
+
+
+        static int[] PointStart(int pos, int[] input) {
+            int[] coll = { input[pos] };
+            for (int checkPoint = pos + 1; checkPoint < input.Length; checkPoint++) {
+                coll = IterateColl(pos, input, coll);
+            }
+            
+            return coll;
+        }
+
+        static int[] IterateColl(int startPoint, int[] input, int[] collection) {
+            for (int i = startPoint; i < input.Length; i++) {
+                  if (input[i] > collection[collection.Length - 1]) {
+                    collection = collection.Append(input[i]);
+                    collection = IterateColl(i, input, collection);
+                  }
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// Prints the Array Number and the Array Contents.
+        /// </summary>
+        /// <param name="pos">Input Array Number.</param>
+        /// <param name="arr">Input Array Data.</param>
+        static void PrintArray(int pos, int[] arr) {
+            Console.Write(pos + ": ");
+            for (int i = 0; i < arr.Length; i++) {
+                if (i == arr.Length - 1) {
+                    Console.Write(arr[i]);
+                    continue;
+                }
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    //https://www.techieclues.com/blogs/how-to-add-elements-to-an-array-in-csharp
+    public static class ExtensionClass
+    {
+        // Extension method to append the element
+        public static T[] Append<T>(this T[] array, T item)
+        {
+            List<T> list = new List<T>(array);
+            list.Add(item);
+
+            return list.ToArray();
+        }
+    }
+}
