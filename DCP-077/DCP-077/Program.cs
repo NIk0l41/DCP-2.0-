d4a0bd09dccdf2e32bcbfa5b49897054c98e7edc
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-10 19:20:45 +1000

    DCP-077
    Man!
    What a fever dream.
    Added:
    - ReturnSortedIntervalCollection()
    - ReturnMergedIntervalCollection()
    Also:
    - Remade MergeOverlappingIntervals() to not check, merely execute since other checks would be done prior.
    
    What a great Coding Challenge!
    Kept me busy the whole day :P

--- DCP-077/DCP-077/Program.cs
+++ DCP-077/DCP-077/Program.cs
@@ -2,7 +2,7 @@
 
 namespace DCP_077
 {
-    class Program
+    public partial class Program
     {
         static void Main(string[] args)
         {
@@ -28,144 +28,137 @@ namespace DCP_077
             int[,] intervalA = { { 1, 3 }, { 5, 8 }, { 4, 10 }, { 20, 25 } };
             int[,] intervalB = { { 20, 25 }, { 5, 8 }, { 1, 3 }, { 4, 10 } };
             int[,] intervalC = { { 1, 5 }, {4, 10 } };
-            Print(0);
-            //We need to print out the contents of integer arrays.
+            //Print Original Contents
             PrintTitle("Interval Collection C");
             PrintIntervalCollection(intervalC);
+            //Print Sorted Contents
+            PrintTitle("Sorted Interval Collection C");
+            PrintIntervalCollection(ReturnSortedIntervalCollection(intervalC));
+            //Print Merged Contents.
             PrintTitle("Merged Interval Collection C");
-            PrintIntervalCollection(ReturnMergedIntervals(intervalC));
+            PrintIntervalCollection(ReturnMergedIntervalCollection(intervalC));
         }
 
-
-
-        /*
-        int[,] OrderIntervalArray(int[,] inputArray) {
-            int[,] intervalOrdered = new int[inputArray.Length / 2, 2];
-            intervalOrdered[0, 0] = inputArray[0, 0];
-            intervalOrdered[0, 1] = inputArray[0, 1];
-            for (int intervalSlot = 1; intervalSlot < inputArray.Length / 2; intervalSlot++) {
-                if (inputArray[intervalSlot - 1, 0] > inputArray[intervalSlot, 0]) {
-                    intervalOrdered
+        /// <summary>
+        /// Sorts a collection of intervals by their first index.
+        /// </summary>
+        /// <param name="input">A Collection of Number Intervals.</param>
+        /// <returns>A sorted collection of number Intervals.</returns>
+        static int[,] ReturnSortedIntervalCollection(int[,] input) {
+            /// 1) One Sort.
+            for (int intervalCollection = 0; intervalCollection < (input.Length / 2) - 1; intervalCollection++) {
+                if (input[intervalCollection, 0] > input[intervalCollection + 1, 0]) {
+                    //T = A
+                    int[] tmp = { input[intervalCollection, 0], input[intervalCollection, 1]};
+                    //A = B
+                    input[intervalCollection, 0] = input[intervalCollection + 1, 0];
+                    input[intervalCollection, 1] = input[intervalCollection + 1, 1];
+                    //B = T
+                    input[intervalCollection + 1, 0] = tmp[0];
+                    input[intervalCollection + 1, 1] = tmp[1];
                 }
             }
-        }*/
 
-        /// <summary>
-        /// PrintShit
-        /// </summary>
-        /// <param name="num"></param>
-        /// <param name="context"></param>
-        static void Print(int num, string context = "") {
-            Console.WriteLine(context + num);
-        }
-
-        /// <summary>
-        /// Print More Shit.
-        /// </summary>
-        /// <param name="interval"></param>
-        static void PrintInterval(int[] interval) {
-            Console.WriteLine(interval[0] + ", " + interval[1]);
-        }
-
-        /// <summary>
-        /// Prints a formatted string for titles.
-        /// </summary>
-        /// <param name="title">Title to Print</param>
-        static void PrintTitle(string title) {
-            Console.WriteLine("---" + title + "---");
-        }
-
-        /// <summary>
-        /// Prints a formatted string for subtitles.
-        /// </summary>
-        /// <param name="subtitle">Subtitle to Print</param>
-        static void PrintSubTitle(string subtitle)
-        {
-            Console.WriteLine("-" + subtitle + "-");
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="input"></param>
-        static void PrintIntervalCollection(int[,] input) {
-            for (int intervalSlot = 0; intervalSlot < input.Length / 2; intervalSlot++) {
-                Console.Write("{ " + input[intervalSlot, 0] + ", " + input[intervalSlot, 1] + " }");
-                if (intervalSlot != (input.Length / 2) - 1) {
-                    Console.Write(",");
+            /// 2) Check for completion.
+            //If one sort, didn't work, we iterate again.
+            //First, we add a counter.
+            int counter = 0;
+            for (int intervalCollection = 0; intervalCollection < (input.Length / 2) - 1; intervalCollection++) {
+                if (input[intervalCollection, 0] > input[intervalCollection + 1, 0]) {
+                    counter++;
                 }
-                Console.WriteLine();
             }
-            Console.ReadLine();
+
+            /// 3) Iterate if incomplete.
+            //If this counter is > 0, then the sort is incomplete, and we need to iterate.
+            //Eventually, this should sort the entire array.
+            //Through brute force. It's not efficient,
+            //but it'll get there eventually with larger datasets.
+            if (counter > 0) {
+                input = ReturnSortedIntervalCollection(input);
+            }
+
+            /// 4)Return the sorted array!
+            return input;
         }
 
         /// <summary>
-        /// Input an N x 2 array with number intervals and returns it with no overlaps.
+        /// Scans for an overlap, merges a single overlap, then creates a new collection with one less item.
+        /// It iterates with itself to check for other overlaps then returns the result.
         /// </summary>
-        /// <param name="input">A 2D array with a collection of number intervals.</param>
-        /// <returns>A modified input array with no overlapping intervals.</returns>
-        static int[,] ReturnMergedIntervals(int[,] input) {
-            //First, create a tmp variable of non-overlapping intervals.
-            int[,] tmp = new int[input.Length / 2, 2];
-            //Then, we create another blank variable which will act as the output.
-            int[,] output;
-            //First, we have to create an overlap finder.
+        /// <param name="input">A Sorted Number Interval Collection. Use "SortedIntervalCollection" first if you haven't already.</param>
+        /// <returns>A new Number Interval Collection with no numerial overlaps.</returns>
+        static int[,] ReturnMergedIntervalCollection(int[,] input) {
+            /// 1) Scan for a single overlap, FUNCTION then break
+            //We create a bool.
+            //Later, if it's true, then a merge has taken place.
+            bool hasMergeOccurred = false;
             for (int intervalSlot = 0; intervalSlot < (input.Length / 2) - 1; intervalSlot++) {
-                //If A(1) > B(0)
-                if (input[intervalSlot, 1] < input[intervalSlot + 1, 0])
-                {
-                    //We have found an overlap.
-                    //So, we need to merge it.
-                    int[] mergedInterval = MergeOverlappingIntervals(
-                        new int[] {
-                            input[intervalSlot, 0],
-                            input[intervalSlot, 1]
-                            },
-                        new int[]{
-                            input[intervalSlot + 1, 0],
-                            input[intervalSlot + 1, 1]
-                        } 
-                    );
-                    //Now that we've merged that interval, we
-                    //Need to declare that B = mergedInterval.
-                    //That way, the process isn't interrupted.
-                    PrintInterval(mergedInterval);
-                    input[intervalSlot + 1, 0] = mergedInterval[0];
-                    input[intervalSlot + 1, 1] = mergedInterval[1];
-                    
-                }
-                else {
-                    //If there is no overlap, A is added to the tmp array.
-                    tmp[intervalSlot, 0] = input[intervalSlot, 0];
-                    tmp[intervalSlot, 1] = input[intervalSlot, 1];
-                    PrintSubTitle("tmp");
-                    Print(tmp[intervalSlot, 0], "tmp[" + intervalSlot + ", 0] = ");
-                    Print(tmp[intervalSlot, 1], "tmp[" + intervalSlot + ", 1] = ");
-                }
-            }
-            //PrintTitle("tmp");
-            //PrintIntervalCollection(tmp);
-            //Once all the merging has been done, we need to create a clean output array.
-            output = new int[tmp.Length / 2, 2];
-            //Then, we append the appropriate items into their respective slots.
-            for (int intervalSlot = 0; intervalSlot < output.Length / 2; intervalSlot++) {
-                output[intervalSlot, 0] = tmp[intervalSlot, 0];
-                output[intervalSlot, 1] = tmp[intervalSlot, 1];
-            }
-            //Now we can return the output variable!
-            return output;
-        }
-
-        static int[,] ReturnMerged2(int[,] input) {
-            int[,] output = new int[input.Length / 2, 2];
-            for (int intervalSlot = 0; intervalSlot < (input.Length / 2); intervalSlot++) {
                 //A(1) > B(0)
-                if (input[intervalSlot, 1] > input[intervalSlot + 1, 0]) {
-                    int merged = MergeOverlappingIntervals();
-                    output[intervalSlot, ]
+                if (input[intervalSlot, 1] > input[intervalSlot + 1, 0]){
+                    /// 2) Merge overlap. Place Merge in A. Erase B.
+                    int[] tmp = MergeOverlappingIntervals(
+                        new int[] { input[intervalSlot, 0], input[intervalSlot, 1] },
+                        new int[] { input[intervalSlot + 1, 0], input[intervalSlot + 1, 1] }
+                        );
+                    //Place tmp in A's position.
+                    input[intervalSlot, 0] = tmp[0];
+                    input[intervalSlot, 1] = tmp[1];
+                    //Erase B by multiplying B(0) by -1.
+                    //No collection of number intervals is in the negative!
+                    //This will be a pointer that it has no place in the future array, and to be ignored.
+                    input[intervalSlot + 1, 0] *= -1;
+                    //We also need to let the algorithm know that a merge has taken place.
+                    //(Forgot to add this on my first run through of this finished script :P)
+                    hasMergeOccurred = true;
+                    break;
                 }
             }
-            return output;
+            //PrintSubTitle("After Merge");
+            //PrintIntervalCollection(input);
+            /// 3) Create a new Interval Collection with the remnants of input.
+            //We run this script if a merge has occurred.
+            if (hasMergeOccurred) {
+                //First, we create our new 'Merged Interval Collection', or MIC
+                //It needs to be one shorter than before, to account for the merge.
+                int[,] MIC = new int[(input.Length / 2) - 1, 2];
+                //Print(MIC.Length, "MIC LENGTH");
+                //Keeps track of where data is inserted into the MIC.
+                //Stands for 'Merged Interval Collection Interval Slot'
+                int MICIS = 0;
+                for (int intervalSlot = 0; intervalSlot < (input.Length / 2); intervalSlot++) {
+                    //If we find an item that needs to be erased,
+                    //We simply skip over it.
+                    if (input[intervalSlot, 0] < 0) {
+                        continue;
+                    }
+                    //If that item needs to be added, we add it.
+                    //Using MICIS and intervalSlot, we can place the data in the correct location.
+                    MIC[MICIS, 0] = input[intervalSlot, 0];
+                    MIC[MICIS, 1] = input[intervalSlot, 1];
+                    //We then add 1 to MICIS to indicate the next slot that needs to be filled.
+                    //This compensates for the shifting that occurs in the dataset.
+                    //FORGOT TO FUCKING ADD 1.
+                    //GOD DAMN IT.
+                    MICIS++;
+                    //WROTE THE COMMENT NOT THE CODE.
+                    //NOTE: IF THIS IS NOT THERE, MANY ZEROES.
+                }
+
+                /// 4) Iterate with self.
+                // This iteration allows us to offload if there are more overlaps and merges to take place,
+                // since this function only does a single merge.
+                //PrintSubTitle("MIC");
+                //PrintIntervalCollection(MIC);
+                MIC = ReturnMergedIntervalCollection(MIC);
+
+                /// 5) Return Merged Interval Collection.
+                // After all that, we can now return our final product!
+                return MIC;
+            }
+
+            //If no merge occurred, then we return the input!
+            return input;
         }
 
         /// <summary>
@@ -176,12 +169,7 @@ namespace DCP_077
         /// <returns>A merged interval where the first item is the lowest of the two, and the second item is the highest of the two.</returns>
         static int[] MergeOverlappingIntervals(int[] inputA, int[] inputB) {
             int[] output;
-            if (inputA[1] > inputB[0]) {
-                output = new int[] { Math.Min(inputA[0], inputB[0]), Math.Max(inputA[1], inputB[1]) };
-            }
-            else {
-                output = inputB;
-            }
+            output = new int[] { Math.Min(inputA[0], inputB[0]), Math.Max(inputA[1], inputB[1])};
             return output;
         }
     }
---
+++ DCP-077/DCP-077/Program2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCP_077
+{
+    public partial class Program
+    {
+
+        /// <summary>
+        /// PrintShit
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="context"></param>
+        static void Print(int num, string context = "")
+        {
+            Console.WriteLine(context + num);
+        }
+
+        /// <summary>
+        /// Print More Shit.
+        /// </summary>
+        /// <param name="interval"></param>
+        static void PrintInterval(int[] interval)
+        {
+            Console.WriteLine(interval[0] + ", " + interval[1]);
+        }
+
+        /// <summary>
+        /// Prints a formatted string for titles.
+        /// </summary>
+        /// <param name="title">Title to Print</param>
+        static void PrintTitle(string title)
+        {
+            Console.WriteLine("---" + title + "---");
+        }
+
+        /// <summary>
+        /// Prints a formatted string for subtitles.
+        /// </summary>
+        /// <param name="subtitle">Subtitle to Print</param>
+        static void PrintSubTitle(string subtitle)
+        {
+            Console.WriteLine("-" + subtitle + "-");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        static void PrintIntervalCollection(int[,] input)
+        {
+            for (int intervalSlot = 0; intervalSlot < input.Length / 2; intervalSlot++)
+            {
+                Console.Write("{ " + input[intervalSlot, 0] + ", " + input[intervalSlot, 1] + " }");
+                if (intervalSlot != (input.Length / 2) - 1)
+                {
+                    Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// (OBSELETE) Input an N x 2 array with number intervals and returns it with no overlaps.
+        /// </summary>
+        /// <param name="input">A 2D array with a collection of number intervals.</param>
+        /// <returns>A modified input array with no overlapping intervals.</returns>
+        static int[,] OBSELETEReturnMergedIntervals(int[,] input)
+        {
+            //First, create a tmp variable of non-overlapping intervals.
+            int[,] tmp = new int[input.Length / 2, 2];
+            //Then, we create another blank variable which will act as the output.
+            int[,] output;
+            //First, we have to create an overlap finder.
+            for (int intervalSlot = 0; intervalSlot < (input.Length / 2) - 1; intervalSlot++)
+            {
+                //If A(1) > B(0)
+                if (input[intervalSlot, 1] < input[intervalSlot + 1, 0])
+                {
+                    //We have found an overlap.
+                    //So, we need to merge it.
+                    int[] mergedInterval = MergeOverlappingIntervals(
+                        new int[] {
+                            input[intervalSlot, 0],
+                            input[intervalSlot, 1]
+                            },
+                        new int[]{
+                            input[intervalSlot + 1, 0],
+                            input[intervalSlot + 1, 1]
+                        }
+                    );
+                    //Now that we've merged that interval, we
+                    //Need to declare that B = mergedInterval.
+                    //That way, the process isn't interrupted.
+                    PrintInterval(mergedInterval);
+                    input[intervalSlot + 1, 0] = mergedInterval[0];
+                    input[intervalSlot + 1, 1] = mergedInterval[1];
+
+                }
+                else
+                {
+                    //If there is no overlap, A is added to the tmp array.
+                    tmp[intervalSlot, 0] = input[intervalSlot, 0];
+                    tmp[intervalSlot, 1] = input[intervalSlot, 1];
+                    PrintSubTitle("tmp");
+                    Print(tmp[intervalSlot, 0], "tmp[" + intervalSlot + ", 0] = ");
+                    Print(tmp[intervalSlot, 1], "tmp[" + intervalSlot + ", 1] = ");
+                }
+            }
+            //PrintTitle("tmp");
+            //PrintIntervalCollection(tmp);
+            //Once all the merging has been done, we need to create a clean output array.
+            output = new int[tmp.Length / 2, 2];
+            //Then, we append the appropriate items into their respective slots.
+            for (int intervalSlot = 0; intervalSlot < output.Length / 2; intervalSlot++)
+            {
+                output[intervalSlot, 0] = tmp[intervalSlot, 0];
+                output[intervalSlot, 1] = tmp[intervalSlot, 1];
+            }
+            //Now we can return the output variable!
+            return output;
+        }
+
+    }
+}
