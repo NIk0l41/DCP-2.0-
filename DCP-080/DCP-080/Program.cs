5a28692d2ec1b041185dc4763e38b7d32fce3331
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-13 08:51:09 +1000

    DCP-080
    Took me a while to do this fully!
    There's a lot of iterations, but I got there after an hour. Pretty happy since I don't have a great track record with Binary Trees.
    >Achievement Get!<
    
    Added:
    - BinaryTree Class
    - ReturnDeepestNode()
    - BranchLength()
    - ReturnBranchLength()

---
+++ DCP-080/DCP-080/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DCP_080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binTree = new BinaryTree(
+                'a',
+                new BinaryTree(
+                    'b',
+                    new BinaryTree('d'),
+                    new BinaryTree('e',
+                        new BinaryTree('f')
+                        )
+                    ),
+                new BinaryTree('c')
+            );
+
+            Console.WriteLine(ReturnDeepestNode(binTree).data);
+        }
+
+        /// <summary>
+        /// If a tree has no branches, then it is the furthest node, and it returns that.
+        /// If it has branches, then the function iterates on the branch with more nodes.
+        /// </summary>
+        /// <param name="tree">Input Binary Tree.</param>
+        /// <returns>The node furthest from the initial node.</returns>
+        static BinaryTree ReturnDeepestNode(BinaryTree tree) {
+            /// 1) If tree has no branches, return tree.
+            int lLen = tree.BranchLength()[0];
+            int rLen = tree.BranchLength()[1];
+            int len = rLen + lLen;
+            if (len == 0)
+                return tree;
+            /// 2) If tree has branches, select the one with more branches
+            /// and iterate ReturnDeepestNode(tree)
+            tree = (lLen > rLen) ? tree.left : tree.right;
+            tree = ReturnDeepestNode(tree);
+            return tree;
+        }
+    }
+}
