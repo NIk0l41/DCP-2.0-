170d69ffb0a00e70efa4c52ac3ebcfb38cb03291
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-19 10:29:14 +1000

    DCP-085
    Done!
    - Add NumberOfParenthesisToRemoveUntilValid()

---
+++ DCP-086/DCP-086/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DCP_086
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paran = "()(())((()";
+            char[] pInput = paran.ToCharArray();
+            Console.WriteLine(NumberOfParanthesisToRemoveUntilValid(pInput));
+        }
+
+        static int NumberOfParanthesisToRemoveUntilValid(char[] input) {
+            /// 1) Setup Variables
+            int unPairedOpens = 0;
+            int unPairedCloses = 0;
+            int totalPairs = 0;
+
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] == '(') {
+                    unPairedOpens++;
+                    continue;
+                }
+                if (input[i] == ')') {
+                    if (unPairedOpens == 0) {
+                        unPairedCloses++;
+                        continue;
+                    }
+                    else {
+                        unPairedOpens--;
+                        totalPairs++;
+                        continue;
+                    }
+                }
+            }
+            return unPairedCloses + unPairedOpens;
+        }
+
+        //static string[] CullNullStringsInArray(string[] input) {
+        //    bool cullOccurs = false;
+        //    int cullSlot = -1;
+        //    string[] arrOut = new string[input.Length - 1];
+        //    for (int i = 0; i < input.Length; i++) {
+        //        if (input[i] == null || input[i] == string.Empty) {
+        //            cullOccurs = true;
+        //            cullSlot = i;
+        //            break;
+        //        }
+        //    }
+        //    if (cullOccurs) {
+        //        for (int i = cullSlot; i < arrOut.Length - 1; i++) {
+        //            input[i] = input[i + 1];
+        //        }
+        //        for (int i = 0; i < arrOut.Length; i++) {
+        //            arrOut[i] = input[i];
+        //        }
+        //        return CullNullStringsInArray(arrOut);
+        //    }
+        //    else {
+        //        return input;
+        //    }
+        //}
+    }
+}
