56e9d08550ece684cd9ffcf19c8f25e9292d5bfa
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-11-21 14:41:57 +1000

    DCP-088 (Done)
    Create QuotientOf()
    That was pretty straight forward ngl, but it was a fun proof of concept! :D

---
+++ DCP-088/DCP-088/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DCP_088
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quotient = QuotientOf(1, 4);
+            Console.WriteLine(quotient);
+        }
+
+        /// <summary>
+        /// Determines whether the quotient would be < 1 or >= 1, then appropriately 'divides' the dividend by the divisor.
+        /// </summary>
+        /// <param name="dividend"></param>
+        /// <param name="divisor"></param>
+        /// <returns>An Integer Quotient.</returns>
+        static int QuotientOf(int dividend, int divisor) {
+            int output = 0;
+            //If the quotient would be < 1
+            if (dividend < divisor) {
+                //Round up or round down?
+                output = (dividend < (divisor / 2)) ? 0 : 1;
+            }
+            //If the quotient would be >= 1
+            while (dividend >= divisor) {
+                output++;
+                dividend -= divisor;
+            }
+            return output;
+        }
+    }
+}
